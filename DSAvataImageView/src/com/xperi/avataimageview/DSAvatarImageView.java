b8b2d59a0faf19d8a76d26a787375b99bfb47d75
Yang dong seok <xperi@naver.com>
2015-01-06 13:15:02 +0900

    init

---
+++ DSAvataImageView/src/com/xperi/avataimageview/DSAvatarImageView.java
@@ -0,0 +1,184 @@
+package com.xperi.avataimageview;
+
+import java.util.StringTokenizer;
+
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Bitmap;
+import android.graphics.BitmapShader;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.text.TextPaint;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class DSAvatarImageView extends ImageView{
+
+
+	private int borderWidth;
+	private int canvasSize;
+	private String name;
+	private Bitmap image;
+	private Paint paint;
+	private Paint paintBorder;
+	private TextPaint paintText;
+
+	public DSAvatarImageView(Context context) {
+		this(context,null);
+	}
+
+	public DSAvatarImageView(Context context, AttributeSet attrs) {
+		this(context, attrs,R.attr.avataImageViewStyle);
+	}
+	public DSAvatarImageView(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+
+		paint = new Paint();
+		paint.setAntiAlias(true);
+
+		paintBorder = new Paint();
+		paintBorder.setAntiAlias(true);
+
+		paintText=new TextPaint(Paint.ANTI_ALIAS_FLAG);
+
+		TypedArray attributes = context.obtainStyledAttributes(attrs, R.styleable.DSAvatarImageView, defStyle, 0);
+		if(attributes.getBoolean(R.styleable.DSAvatarImageView_border, true)) {
+			int defaultBorderSize = (int) (4 * getContext().getResources().getDisplayMetrics().density + 0.5f);
+			setBorderWidth(attributes.getDimensionPixelOffset(R.styleable.DSAvatarImageView_border_width, defaultBorderSize));
+			setBorderColor(attributes.getColor(R.styleable.DSAvatarImageView_border_color, Color.GRAY));
+			setName(attributes.getString(R.styleable.DSAvatarImageView_name));
+		}
+	}
+
+
+	public void setBorderWidth(int borderWidth) {
+		this.borderWidth = borderWidth;
+		this.requestLayout();
+		this.invalidate();
+	}
+	public void setName(String name) {
+		this.name = name;
+		this.requestLayout();
+		this.invalidate();
+	}
+
+	public void setBorderColor(int borderColor) {
+		if (paintBorder != null)
+			paintBorder.setColor(borderColor);
+		this.invalidate();
+	}
+
+
+	@Override
+	public void onDraw(Canvas canvas) {
+		image = drawableToBitmap(getDrawable());
+
+		canvasSize = canvas.getWidth();
+		if(canvas.getHeight()<canvasSize)
+			canvasSize = canvas.getHeight();
+
+		int circleCenter = (canvasSize - (borderWidth * 2)) / 2;
+
+		canvas.drawCircle(circleCenter + borderWidth, circleCenter + borderWidth, ((canvasSize - (borderWidth * 2)) / 2) + borderWidth - 4.0f, paintBorder);
+
+		if (image != null) {
+			BitmapShader shader = new BitmapShader(Bitmap.createScaledBitmap(image, canvasSize, canvasSize, false), Shader.TileMode.CLAMP, Shader.TileMode.CLAMP);
+			paint.setShader(shader);
+			canvas.drawCircle(circleCenter + borderWidth, circleCenter + borderWidth, ((canvasSize - (borderWidth * 2)) / 2) - 4.0f, paint);
+		}
+
+		if(name!=null){
+			String text=getInitialByName(name);
+			paintText.setColor(Color.WHITE);
+			paintText.setTextSize(Math.max(Math.min(((canvasSize-borderWidth*4)/paint.measureText("AA"))*10, Float.MAX_VALUE), 0));
+			Rect textBound = new Rect();
+			paintText.getTextBounds(text, 0, text.length(), textBound);
+			canvas.drawText(text, circleCenter+borderWidth/2-textBound.width()/2, circleCenter+borderWidth+textBound.height()/2, paintText);
+		}
+
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		int width = measureWidth(widthMeasureSpec);
+		int height = measureHeight(heightMeasureSpec);
+		setMeasuredDimension(width, height);
+	}
+
+	private int measureWidth(int measureSpec) {
+		int result = 0;
+		int specMode = MeasureSpec.getMode(measureSpec);
+		int specSize = MeasureSpec.getSize(measureSpec);
+
+		if (specMode == MeasureSpec.EXACTLY) {
+			result = specSize;
+		} else if (specMode == MeasureSpec.AT_MOST) {
+			result = specSize;
+		} else {
+			result = canvasSize;
+		}
+		return result;
+	}
+
+	private int measureHeight(int measureSpecHeight) {
+		int result = 0;
+		int specMode = MeasureSpec.getMode(measureSpecHeight);
+		int specSize = MeasureSpec.getSize(measureSpecHeight);
+
+		if (specMode == MeasureSpec.EXACTLY) {
+			result = specSize;
+		} else if (specMode == MeasureSpec.AT_MOST) {
+			result = specSize;
+		} else {
+			result = canvasSize;
+		}
+		return (result + 2);
+	}
+
+	public Bitmap drawableToBitmap(Drawable drawable) {
+		if (drawable == null) {
+			return null;
+		} else if (drawable instanceof BitmapDrawable) {
+			return ((BitmapDrawable) drawable).getBitmap();
+		}
+
+		Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(),
+				drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+		Canvas canvas = new Canvas(bitmap);
+		drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+		drawable.draw(canvas);
+
+		return bitmap;
+	}
+	public static String getInitialByName(String name){
+		StringBuffer buffer=new StringBuffer();
+		if(name!=null){
+			StringTokenizer t=new StringTokenizer(name," ");
+			System.out.println(t.countTokens());
+			if(t.countTokens()>0){
+				String firstWord=t.nextToken();
+				if(firstWord.length()>0){
+					buffer.append(firstWord.substring(0, 1));
+				}
+				if(t.countTokens()>=1){
+					String lastWord=t.nextToken();
+					if(lastWord.length()>0){
+						buffer.append(lastWord.substring(0, 1));
+					}
+				}
+
+			}
+		}
+		String result=buffer.toString();
+		return result.toUpperCase();
+	}
+
+
+
+}
---
+++ README.md
@@ -0,0 +1,53 @@
+DSAvataImageView
+================
+Subclass of ImageView to create a circle avatar image view with user name initial image view.
+
+![screenshot](https://cloud.githubusercontent.com/assets/7636468/5624104/0bf3d9f8-95a3-11e4-99f3-92afa3fb7efe.png)
+
+Usage
+--------
+
+###XML (Use Circle avatar image view)
+```xml
+    <com.xperi.avataimageview.DSAvatarImageView
+        android:layout_width="250dp"
+        android:layout_height="250dp"
+        android:src="@drawable/image"
+        app:border_color="#EEEEEE"
+        app:border_width="5dp"
+        />
+```
+###XML (Use user name initial view) output 'SJ'
+```xml
+    <com.xperi.avataimageview.DSAvatarImageView
+        android:layout_width="250dp"
+        android:layout_height="250dp"
+        app:border_color="#EEEEEE"
+        app:border_width="5dp"
+        app:name="steve jobs"
+        />
+```
+You may use the following properties in your XML to customize your DSAvataImageView.
+
+#####Properties:
+
+* `app:border`       (boolean)             -> default true
+* `app:border_color` (color)               -> default GRAY
+* `app:border_width` (dimension)           -> default 2dp
+* `app:name`          (String)             -> default 
+
+###JAVA (Use Circle avatar image view)
+
+```java
+    DSAvatarImageView avataImageView=getView(convertView, R.id.circularIv);
+    avataImageView.setBorderColor(getResources().getColor(R.color.GrayLight));
+    avataImageView.setBorderWidth(10);
+    avataImageView.setImageResource(R.id.sample);
+```
+###JAVA (Use user name initial view) output 'SJ'
+
+```java
+    DSAvatarImageView avataImageView=getView(convertView, R.id.circularIv);
+    avataImageView.setBorderColor(getResources().getColor(R.color.GrayLight));
+    avataImageView.setName("steve jobs")
+```
